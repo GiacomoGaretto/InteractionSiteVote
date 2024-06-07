f497e8cc924977f782b4230519befda2cc5c02fc
GiacomoGaretto <124433834+GiacomoGaretto@users.noreply.github.com>
2024-06-07 10:47:42 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let animatingPallini = [];
 const animationDuration = 1500; // 1.5 seconds
 const targetAreaSize = 200; // 20px x 20px area
 
-const ws = new WebSocket('ws://localhost:3000');
+const ws = new WebSocket('ws://voting-ws.glitch.me');
 
 ws.onopen = function() {
     console.log('Connected to the server');
