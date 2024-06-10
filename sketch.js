05834e66fa00e19fb24e5a29340236f6831acddb
GiacomoGaretto <124433834+GiacomoGaretto@users.noreply.github.com>
2024-06-10 09:59:30 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let animatingPallini = [];
 const animationDuration = 1500; // 1.5 seconds
 const targetAreaSize = 200; // 20px x 20px area
 
-const ws = new WebSocket('ws://voting-ws.glitch.me');
+const ws = new WebSocket('ws://192.168.1.190:8081');
 
 ws.onopen = function() {
     console.log('Connected to the server');
@@ -86,7 +86,6 @@ function handleCommand(command) {
     }
 }
 
-
 function preload() {
     customFont = loadFont('FONT/ABCDiatypeRoundedEdu-Medium.woff');
 }
@@ -100,6 +99,21 @@ function setup() {
     document.addEventListener('keydown', keyPressed);
     clear();
     console.log('Setup complete');
+
+    resizeCanvasToContainer();
+
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+}
+
+function resizeCanvasToContainer() {
+    let canvasContainer = document.getElementById('canvas-container');
+    resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+}
+
+function handleFullscreenChange() {
+    if (!document.fullscreenElement) {
+        resizeCanvasToContainer();
+    }
 }
 
 function draw() {
@@ -108,9 +122,14 @@ function draw() {
     // Disegna la scritta con il font personalizzato e il colore specificato
     fill('#000000'); // Sostituisci con il colore desiderato
     textFont(customFont);
-    textSize(80);
+    textSize(150);
     textAlign(LEFT, TOP);
-    text('PREFERISCI SUSHI O PIZZA?', 25, 10);
+    text('SUSHI o PIZZA?', 25, 40);
+
+    textFont(customFont);
+    textSize(35);
+    textAlign(LEFT, TOP);
+    text('GIORNO 1', 25, 15);
 
     fill(0);
     textSize(10);
@@ -134,11 +153,6 @@ function draw() {
             animatingPallini = animatingPallini.filter(a => a !== anim); // Rimuovi l'animazione completata
         }
     }
-}
-
-function resizeCanvasToContainer() {
-    let canvasContainer = document.getElementById('canvas-container');
-    resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
 }
 
 function addPallini() {
@@ -249,6 +263,7 @@ class Pallino {
         if (this.y - this.diametro / 2 < 0 || this.y + this.diametro / 2 > height) {
             this.velocita.y *= -1;
         }
+        
     }
 
     display() {
@@ -260,6 +275,13 @@ class Pallino {
         this.colore = c;
         // Aggiorna il diametro quando viene cambiato il colore
         this.diametro = (this.colore.toString() === color(217).toString()) ? 7 : 9.6;
+
+        // Aggiorna i contatori dei pallini
+        if (this.colore.toString() === color(0, 148, 255).toString()) {
+            numBlu++;
+        } else if (this.colore.toString() === color(255, 156, 40).toString()) {
+            numArancioni++;
+        }
     }
 
     getColor() {
@@ -267,29 +289,45 @@ class Pallino {
     }
 
     flock(pallini) {
-        // Skip flocking for gray dots
-        if (this.colore.toString() === color(217).toString()) {
-            return;
-        }
-
-        // Area di attrazione per i pallini blu e arancioni
+        // Definizione delle aree di attrazione
         let targetX, targetY;
-
+        let areaSize = targetAreaSize;
+    
         if (this.colore.toString() === color(0, 148, 255).toString()) {
-            targetX = width / 4 + random(-targetAreaSize / 2, targetAreaSize / 2);
-            targetY = height / 2 + random(-targetAreaSize / 2, targetAreaSize / 2);
-        } else {
-            targetX = 3 * width / 4 + random(-targetAreaSize / 2, targetAreaSize / 2);
-            targetY = height / 2 + random(-targetAreaSize / 2, targetAreaSize / 2);
+            // Area di attrazione per i pallini blu
+            if (numGrigi === 0) {
+                areaSize += 0.5 * Math.floor(numBlu);
+                targetX = width / 4 + random(-targetAreaSize / 2, targetAreaSize / 2);
+                targetY = height / 2 + random(-targetAreaSize / 2, targetAreaSize / 2);
+            } else {
+                areaSize += 0.5 * Math.floor(numBlu);
+                targetX = width / 5 + random(-areaSize / 2, areaSize / 2);
+                targetY = height / 2 + random(-areaSize / 2, areaSize / 2);
+            }
+        } else if (this.colore.toString() === color(255, 156, 40).toString()) {
+            // Area di attrazione per i pallini arancioni
+            if (numGrigi === 0) {
+                areaSize += 0.5 * Math.floor(numArancioni);
+                targetX = 3 * width / 4 + random(-targetAreaSize / 2, targetAreaSize / 2);
+                targetY = height / 2 + random(-targetAreaSize / 2, targetAreaSize / 2);
+            } else {
+                areaSize += 0.5 * Math.floor(numArancioni);
+                targetX = 4 * width / 5 + random(-areaSize / 2, areaSize / 2);
+                targetY = height / 2 + random(-areaSize / 2, areaSize / 2);
+            }
+        } else if (this.colore.toString() === color(217).toString()) {
+            // Area di attrazione per i pallini grigi
+            targetX = width / 2 + random(-areaSize / 2, areaSize / 2);
+            targetY = height / 2 + random(-areaSize / 2, areaSize / 2);
         }
-
+    
         // Calcoliamo la forza di attrazione verso il target
         let attrazione = createVector(targetX - this.x, targetY - this.y);
         attrazione.setMag(0.1); // Modifica la forza di attrazione
-
+    
         // Applichiamo la forza di attrazione
         this.velocita.add(attrazione);
-
+    
         // Limitiamo la velocità massima
         this.velocita.limit(this.maxSpeed);
     }
